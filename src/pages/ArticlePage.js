8636c2d19cb426fd918b1283a84cb340625bbdd5
Gjergj Lala <g.lala@lalatech.de>
2021-04-18 19:57:55 +0200

    Calling useEffect at the right time

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 
 import ArticlesList   from '../components/ArticlesList';
@@ -9,17 +10,23 @@ function ArticlePage() {
   let { name } = useParams();
   const article = articleContent.find(article => article.name === name);
 
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    setArticleInfo({ upvotes: 3 });
+  },[name]);
+
   if(!article) return <NotFoundPage/>;
 
-  const otherArticles=articleContent.filter(article => article.name !== name)
-    .slice(0, 2);
+  const otherArticles = articleContent.filter(article => article.name !== name).slice(0, 2);
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
-      <h3>Other Articles</h3>
+      <h3>Other Articles:</h3>
       <ArticlesList articles={otherArticles}/>
     </>
   );
